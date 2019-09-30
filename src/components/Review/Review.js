d56f42f255968d1b0a5c3bb6ed8442253afc27dc
Khou Thao <thao.khou@gmail.com>
2019-09-29 22:21:29 -0500

    Added a post  to post to server with data.

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,21 @@
-// fifth page
-
 import React, { Component } from 'react';
 import axios from 'axios';
 import {connect} from 'react-redux';
 
 
 class Review extends Component {
+    //post to server
+    handlePost = () => {
+        console.log(this.props.reduxState.nextReducer)
+        axios.post('/api/feedback', this.props.reduxState.nextReducer)
+        .then( (response) => {
+          console.log(response.data);
+        })
+        .catch( (error) => {
+          console.log('ERROR IN POST', error);
+        })
+      }
+    
     handleClick = () => {
         //go to Completed.js
         this.props.history.push("/completed");
@@ -18,10 +28,10 @@ class Review extends Component {
         </header>
         <br/>
        <div>
-           <h4>Feeling:</h4>
-           <h4>Understanding:</h4>
-           <h4>Support:</h4>
-           <h4>Comments:</h4>
+           <h4>Feeling: {this.props.reduxState.nextReducer.newFeeling}</h4>
+           <h4>Understanding: {this.props.reduxState.nextReducer.newUnderstanding}</h4>
+           <h4>Support: {this.props.reduxState.nextReducer.newSupport}</h4>
+           <h4>Comments: {this.props.reduxState.nextReducer.newComment}</h4>
        </div>
         <button onClick={this.handleClick}>SUBMIT</button>
       </div>
