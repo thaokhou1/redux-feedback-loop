aa20da27773374460e6ff19b529d9ed493cea3fa
Khou Thao <thao.khou@gmail.com>
2019-09-29 22:17:36 -0500

    Added an object of feedback and  updated reducers.

--- src/index.js
+++ src/index.js
@@ -9,17 +9,48 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 
-const nextReducer = (state = {}, action) => {
-    // Add to the order in the store
-    if (action.type === 'SET_REVIEW') {
-      return {...state, ...action.payload};
+const feedback = 
+    {
+        newFeeling: '',
+        newUnderstanding: '',
+        newSupport: '',
+        newComments: ''
     }
-    return state;
+
+const nextReducer = (state = feedback, action) => {
+  //add feedback to store
+  switch (action.type) {
+    case 'ADD_FEELING': 
+        state.newFeeling = action.payload
+        return state;
+    case 'ADD_UNDERSTANDING':
+        state.newUnderstanding = action.payload
+        return state;
+    case 'ADD_SUPPORT':
+        state.newSupport = action.payload;
+        return state;
+    case 'ADD_COMMENT':
+        state.newComment = action.payload;
+        return state;
+    default:
+        return state;
 }
+}
+
+const grabFeedback = (state = [], action) => {
+  if (action.type === 'GET_FEEDBACK') {
+    return action.payload;
+  } 
+  return state;
+}
+
 
 const reduxStore = createStore(
     // use combineReducers in case we add more later
-   nextReducer
+    combineReducers({
+     nextReducer,
+     grabFeedback,
+  }), 
   );
 
   
